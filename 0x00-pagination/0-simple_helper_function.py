d0f48f3408d798487418c99c186793d3e9381d96
Henzz <khenok53@gmail.com>
2024-07-22 00:02:59 +0300

    task 0 done pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+def index_range(page: int, page_size: int) -> tuple[int, int]:
+    """
+        Returns a tuple containing the start and end index of the page
+        for the given pagination parameters.
+
+        vim
+        Copy
+        Page numbers are 1-indexed, i.e. the first page is page 1.
+
+        Args:
+            page (int): The page number (1-indexed).
+            page_size (int): The number of items per page.
+
+        Returns:
+            tuple[int, int]: A tuple containing the start and end
+            index of the page.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
